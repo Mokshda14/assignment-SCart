0710f7377343b7dbc5344e4f5a85f691b621c162
Mokshda14 <moxda.081504@gmail.com>
2020-06-26 14:10:40 +0530

    removed unwanted files

--- src/app/src/app/shared/filters/nbd-product-type-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NbdProductTypeFilterPipe } from './nbd-product-type-filter.pipe';
-
-describe('NbdProductTypeFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NbdProductTypeFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
